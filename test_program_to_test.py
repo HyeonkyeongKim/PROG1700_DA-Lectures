93e17af875a504382446a9f53111ca2b1a7f32cc
Hyeonkyeong Kim <w0521537@nscc.ca>
2025-11-08 10:49:48 -0400

    testing

---
+++ test_program_to_test.py
@@ -0,0 +1,11 @@
+def test_number_assumptions():
+    age = 25
+
+    # check to see if the age is a particular value
+    assert age == 25
+
+    # check to see if the age is over 18
+    assert age > 18
+
+    # check to see if the age is under 100
+    assert age < 100
